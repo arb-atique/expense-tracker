a743f4897a41cde7ce0fcd232bbd83b13c75a2c6
atique <atiq.fba@gmail.com>
2025-07-07 20:32:29 +0400

    settings updated

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--p4q203-q&!4_w5xt_%da+)6rkz_up1!9-34erkqc@&-hlv698'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['expense-tracker-1-wmnm.onrender.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
